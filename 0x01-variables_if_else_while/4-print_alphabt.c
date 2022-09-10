dda3d6a68b6aa03355918cf27d6587bd9d4f800f
Noir <jenemali161@gmail.com>
2022-09-09 21:42:48 -0700

    nested-if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp;
+alp = 'a';
+while (alp <= 'z')
+{
+	if (alp != 'e' && alp != 'q')
+{
+	putchar(alp);
+}
+alp++;
+}
+putchar('\n');
+return (0);
+}
