7f7770cc475b34b3e37f69dfc6e27c0c773ca9f9
Noir <jenemali161@gmail.com>
2022-09-09 22:20:36 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints base 16
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int num = 48;
+
+while (num <= 57)
+{
+putchar(num);
+num++;
+}
+num = 97;
+while (num <= 102)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
