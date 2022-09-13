76cda5099e23a14676a577f0dc3c79031c140e3e
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-13 21:37:04 +0100

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,8 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- * main - entry point
- *
- * print_alphabet_x10 - prints alphabet 10 times
- * @c: Character in ASCII
- * Return: Always 0 (success)
-=======
- * print_alphabet_x10 - prints all lowercase alphabets 10 times
->>>>>>> 45e4b7220ab0fa9b817270bb56d2823b62b1557a
- */
+  * print_alphabet_x10 - prints all lowercase alphabets 10 times
+  *
+  */
 void print_alphabet_x10(void)
 {
 	int i, a;
