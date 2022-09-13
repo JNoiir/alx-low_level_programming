370402bb73a8d75c5fa1c1b3f38bd854e8d88c2d
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-14 00:37:29 +0100

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,3 +5,13 @@
  * @n: starting point
  * Return: void returns nothing.
  */
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	else
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
+}
