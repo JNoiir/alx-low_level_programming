58ff1d7b8b42d2a05218545bdbc0b4c1fd0a8395
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-13 22:20:56 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,3 +4,15 @@
  * @c: Character in ASCII code
  * Return: 1 if uppercase, 0 if not
  */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
