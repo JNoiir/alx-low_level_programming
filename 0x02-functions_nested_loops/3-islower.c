e4eea490303ebfce8ac1327df0618c8c08f54fd6
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-13 22:04:20 +0100

    slower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,3 +4,15 @@
  * @c: The character in ASCII code
  * Return: 0 if yes, 1 if not
  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
