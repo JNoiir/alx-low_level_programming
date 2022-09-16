3dabb754e18c691de1657f7a6945fc4584ce4565
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-16 03:42:26 +0100

    and i see darkness all around me..

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+  *print_most_numbers - Prints most numbers
+  *
+  *
+  *Return: No return
+  *
+  */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		if (num != 50 && num != 52)
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
