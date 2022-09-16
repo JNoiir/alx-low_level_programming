0de104a32864aeae6544a3e62fb6ee8f6dc47a19
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-16 03:37:46 +0100

    everything i do is nobody else concern

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+
+/**
+  * mul - multiplies two numbers
+  *
+  *@a: first number
+  *@b: second number
+  *
+  *Return: Multiplication
+  */
+int mul(int a, int b)
+{
+	int op;
+
+	op = a * b;
+
+
+	return (op);
+}
