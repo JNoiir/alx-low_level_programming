a59d2a5128cc6f4af3106765e948d4bc3f40e069
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-16 03:40:07 +0100

    shey you go come online?

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+  *print_numbers - Prints all numbers
+  *
+  *
+  *
+  *Return: No return
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
