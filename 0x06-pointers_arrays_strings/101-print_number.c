f621b585035175ed69a18f64c20dd76df4600461
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-09-23 02:09:45 +0100

    ha

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
+#include "main.h"
+/**
+ * exponent - x to the power of y
+ * @x: base number
+ * @y: exponent
+ * Description: calcuates x^y
+ * Return: x^y
+ *
+ **/
 
-int main(void)
+int exponent(int x, int y)
 {
-	int n;
-	int a[5];
+	int power;
 
-	a[2] = 1024;
+	power = x;
 
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(&n + 3) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	if (x == 0)
+		return (0);
+	if (y == 0)
+		return (1);
+
+	while (y >= 2)
+	{
+		power  = power * x;
+		y--;
+	}
+	return (power);
+}
+
+/**
+ * print_number - print an int using only _putchar
+ * @number: int to be printed by function
+ *
+ * Return: nothing
+ **/
+
+void print_number(int number)
+{
+	int size, digit, counter, sign;
+
+	sign = 1;
+	digit = 0;
+	size = 1;
+	counter = number;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		sign = -1;
+	}
+
+	for (; counter >= 10 || counter <= -10; size++)
+	{
+		counter = counter / 10;
+	}
+
+	counter = number;
+
+	while (size >= 2)
+	{
+		digit = (counter / exponent(10, size - 1)) * sign;
+		_putchar(digit + '0');
+		counter = counter % exponent(10, size - 1);
+		size--;
+	}
+	_putchar(sign * counter % 10 + '0');
 }
