c7123b853231fda7b87b2291f2a331a3c7cd0bb5
Noir <jenemali161@gmail.com>
2022-09-22 17:24:15 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-	
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; src[j] != '\0'; j++)
-	{										dest[i] = src[j];
-		i++;
-	}
-	dest[i] = src[j];
-	return (dest);
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+	i++;
+for (j = 0; src[j] != '\0'; j++)
+{										dest[i] = src[j];
+	i++;
+}
+dest[i] = src[j];
+return (dest);
 }
