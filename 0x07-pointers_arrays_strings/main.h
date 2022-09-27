0f8a8380262efebc17ec2baee6fb16c33ef071bb
Noir <jenemali161@gmail.com>
2022-09-27 09:33:07 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+
+
