e0b9adbcc38b75eab6242ae483c3d1e7e792462d
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-10-01 01:25:00 +0100

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+  *main - adds two integers togeter
+  *
+  *@argc: number of arguments passed
+  *@argv: an array that consits of arguments passed
+  *Return: 0 - success
+  */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
