5f006552a3ef7fb2768bbe7a5f29c5a94da136d6
JNoiir <102287859+JNoiir@users.noreply.github.com>
2022-10-21 00:33:30 +0100

    include nullifier

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,19 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stddef.h>
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
- */
-
+ * print_list - print all elements of a linked list
+ * @h: pointer to linked list
+ * Return: number of nodes in linked list
+ **/
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	if (h == NULL)
+		return (0);
+	printf("[%u] %s\n", h->len, h->str);
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+	if (h->next != NULL)
+		return (print_list(h->next) + 1);
+	return (1);
 }
